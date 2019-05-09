8aeccddcdb8ffc1746ab93fb619302e4dee661fc
Philipp Wirth <pwirth@student.ethz.ch>
2019-05-09 16:17:17 +0200

    first commit

--- model.py
+++ model.py
@@ -78,7 +78,7 @@ class RNNModel(nn.Module):
         samples = self.sampler(bsz, seq_len)    # (nsamples x bsz x seq_len)
 
         samples_emb = embedded_dropout(self.encoder, samples, dropout=self.dropoute if self.training else 0)
-        samples_emb = self.lockdrop(samples_emb, self.dropouti)
+        #samples_emb = self.lockdrop(samples_emb, self.dropouti)
 
         weights_ih, bias_ih = self.rnn.weight_ih_l0, self.rnn.bias_ih_l0  # only one layer for the moment
         weights_hh, bias_hh = self.rnn.weight_hh_l0, self.rnn.bias_hh_l0
