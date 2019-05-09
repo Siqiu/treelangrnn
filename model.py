f352ac92e99ed03362672b34806ccb2ff024deba
Philipp Wirth <pwirth@student.ethz.ch>
2019-05-09 10:48:06 +0200

    first commit

---
+++ model.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+
+from embed_regularize import embedded_dropout
+from locked_dropout import LockedDropout
+from weight_drop import WeightDrop
+
+from sample import NegativeSampler
+
+class RNNModel(nn.Module):
+    """Container module with an encoder and a recurrent module."""
+
+    def __init__(self, ntoken, ninp, nhid, dropout=0.5, dropouth=0.5, dropouti=0.5, dropoute=0.1, frequencies=None):
+        super(RNNModel, self).__init__()
+        self.lockdrop = LockedDropout()
+        self.idrop = nn.Dropout(dropouti)
+        self.hdrop = nn.Dropout(dropouth)
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+
+        self.rnn = torch.nn.RNN(ninp, nhid, 1, dropout=0)
+        print(self.rnn)
+
+        self.init_weights()
+
+        self.ninp = ninp
+        self.nhid = nhid
+        self.dropout = dropout
+        self.dropouti = dropouti
+        self.dropouth = dropouth
+        self.dropoute = dropoute
+
+        self.nonlinearity = nn.Tanh()
+        self.eps = 1e-6
+        self.nsamples = 10
+        self.ntoken = ntoken
+
+        self.sampler = NegativeSampler(self.nsamples, torch.ones(self.ntoken) if frequencies is None else frequencies)
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, data, return_output=False):
+
+        # need this later on
+        dist_fn = nn.PairwiseDistance(p=2)
+
+        # get batch size and sequence length
+        seq_len, bsz = data.size()
+
+        # process positive samples
+        hidden = self.init_hidden(bsz)      # bsz x nhid
+
+        emb = embedded_dropout(self.encoder, data, dropout=self.dropoute if self.training else 0)
+        emb = self.lockdrop(emb, self.dropouti)
+
+        raw_output, new_hidden = self.rnn(emb, hidden)          # apply single layer rnn
+        raw_output = self.lockdrop(raw_output, self.dropout)    # seq_len x bsz x nhid
+        raw_output = raw_output.view(seq_len, bsz, -1)
+
+        # initialize loss w/ positive terms
+        pos_sample_distances = [dist_fn(raw_output[i-1], raw_output[i]).pow(2) if i > 0 else dist_fn(hidden, raw_output[i]).pow(2) for i in range(seq_len)]
+        raw_output = raw_output.view(seq_len*bsz, -1)
+
+        # we want positive terms in the sum as well
+        sum_of_exp = torch.zeros(seq_len*bsz)
+        for i in range(seq_len):
+            sum_of_exp[i*bsz:(i+1)*bsz] = torch.exp(-pos_sample_distances[i])
+        
+        # init loss
+        loss = sum(pos_sample_distances).sum()
+
+        # process negative samples
+        samples = self.sampler(bsz, seq_len)    # nsamples x (bsz x seq_len)
+
+        samples_emb = embedded_dropout(self.encoder, samples, dropout=self.dropoute if self.training else 0)
+        samples_emb = self.lockdrop(samples_emb, self.dropouti)
+
+        weights_ih, bias_ih = self.rnn.weight_ih_l[0], self.rnn.bias_ih_l[0]  # only one layer for the moment
+        weights_hh, bias_hh = self.rnn.weight_hh_l[0], self.rnn.bias_hh_l[0]
+
+        samples_times_W = torch.nn.functional.linear(samples_emb, weights_ih, bias_ih)
+        hiddens_times_U = torch.nn.functional.linear(raw_output, weights_hh, bias_hh)
+
+        # iterate over samples to update loss
+        for i in range(self.nsamples):
+
+            # compute output of negative samples
+            output = self.nonlinearity(samples_times_W[i] + hiddens_times_U)
+
+            # compute loss term
+            distance = dist_fn(raw_output, output).pow(2)
+            sum_of_exp = sum_of_exp + torch.exp(-distance)
+
+        loss = loss + torch.log(sum_of_exp + self.eps).sum()
+
+        return loss
+
+    def evaluate(self, data):
+
+        dist_fn = nn.PairwiseDistance(p=2)
+
+        # get weights and compute WX for all words
+        weights_ih, bias_ih = self.rnn.weight_ih_l[0], self.rnn.bias_ih_l[0]  # only one layer for the moment
+        weights_hh, bias_hh = self.rnn.weight_hh_l[0], self.rnn.bias_hh_l[0]
+
+        all_words = torch.LongTensor([i for i in range(self.ntoken)]).cuda()
+        all_words = embedded_dropout(self.encoder, all_words, dropout=self.dropoute if self.training else 0)
+
+        all_words_times_W = torch.nn.functional.linear(all_words, weights_ih, bias_ih)
+
+        # iterate over data set and compute loss
+        total_loss, hidden = 0, self.init_hidden(1)
+        for i in range(data.size(0)):
+
+            hidden_times_U = torch.nn.functional.linear(hidden.repeat(self.ntoken, 1), weights_hh, bias_hh)
+            output = self.nonlinearity(all_words_times_W + hidden_times_U)
+
+            distance = dist_fn(hidden, output)
+            softmaxed = torch.nn.functional.log_softmax(distance + self.eps, dim=0)
+            raw_loss = -softmaxed[target].item()
+
+            total_loss += raw_loss / data.size(0)
+
+        return total_loss
+
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        return weight.new(1, bsz, self.nhid)
