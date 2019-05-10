30638aaed30276823a6912314b4dfc3f7649bef5
Philipp Wirth <pwirth@student.ethz.ch>
2019-05-10 14:20:20 +0200

    first commit

--- utils.py
+++ utils.py
@@ -40,6 +40,7 @@ def batchify_sorted(data, bsz, args, nsentences_of_length):
             continue
 
         bound = offset + (nsentences//bsz)*bsz*length
+        print(nsentences, length)
         print(bound)
         batchified = batchify(data[offset:bound], bsz, args, None)
         print(data[offset:offset + (nsentences*length)], batchified)
