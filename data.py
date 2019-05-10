560b0cf8c5ef90f8410c70768c2e1f24b80fd684
Philipp Wirth <pwirth@student.ethz.ch>
2019-05-10 15:26:18 +0200

    first commit

--- data.py
+++ data.py
@@ -126,7 +126,8 @@ def sort_dataset(path):
     with open(path, 'r') as f:
 
         lines = [line.split() for line in f]
-        print([len(line) for line in lines])
+        print(lines[1])
+        #print([len(line) for line in lines])
 
 
 
