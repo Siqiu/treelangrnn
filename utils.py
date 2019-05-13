de6d22009b618db0fe1b6ca8ddc8348f57951a98
Philipp Wirth <pwirth@student.ethz.ch>
2019-05-13 14:08:23 +0200

    first commit

--- utils.py
+++ utils.py
@@ -46,7 +46,8 @@ def batchify_padded(data, bsz, args, ntokens, eos_tokens):
         for j in range(len(sentences)-1):
             print(lengths[j])
             print(sentences[j:j+2])
-            batch[0:lengths[j]][j] = data[sentences[j]:sentences[j+1]].view(lengths[j], 1)
+            print(data[sentences[j]:sentences[j+1]])
+            batch[0:lengths[j]][j] = data[sentences[j]:sentences[j+1]].view(lengths[j], -1)
 
         batches.append(batch)
 
