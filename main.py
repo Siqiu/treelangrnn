ae204416cbd6f8a9c039b26267536d4f678c0387
Philipp Wirth <pwirth@student.ethz.ch>
2019-06-10 10:49:05 +0200

    correct train loss

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def run(args):
 
             #raw_loss = model.train_crossentropy(data, eos_tokens)
             raw_loss, hidden = model(data, binary, hidden)
-            avrg_loss = avrg_loss + total_loss * seq_len
+            avrg_loss = avrg_loss + raw_loss * seq_len
 
             loss = raw_loss
             '''
@@ -256,7 +256,7 @@ def run(args):
             total_loss += loss.data
             optimizer.param_groups[0]['lr'] = lr2
             if batch % args.log_interval == 0 and batch > 0:
-                cur_loss = total_loss.item() / seq_len
+                cur_loss = total_loss.item() / args.log_interval
                 elapsed = time.time() - start_time
                 print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
                         'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
