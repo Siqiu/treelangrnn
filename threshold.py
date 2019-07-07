2ae962c7e3e41545101fc54a6d9785e515d2c7d4
Philipp Wirth <pwirth@lo-login-01.leonhard.ethz.ch>
2019-07-07 11:18:45 +0200

    blu

--- threshold.py
+++ threshold.py
@@ -23,7 +23,7 @@ def soft_threshold2(d, r, inf=1e4):
 	dnew[idxs] = d[idxs]
 	return torch.clamp(dnew, max=inf)
 
-def init_weights(m, a=0., b=0.03):
+def init_weights(m, a=0., b=0):
 	if type(m) == nn.Linear:
 		torch.nn.init.uniform_(m.weight, a, b)
 
